cc93323082226fbb151d00e987658e3e57384250
ronicse <roni_cse@yahoo.com>
2025-08-04 01:30:59 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 st.title("Linear Regression Web Application")
+st.subheader("Data Science with Dr. MS Rahman")
