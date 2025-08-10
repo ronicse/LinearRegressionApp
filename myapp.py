21ec00b330f57e83e7044d2b4480c6d9580ed02f
ronicse <roni_cse@yahoo.com>
2025-08-11 00:10:00 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,6 +5,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
 
 st.title("Linear Regression Web Application")
 st.subheader("Data Science with Dr. MS Rahman")
@@ -43,6 +46,32 @@ features = st.multiselect("Select input feature columns", [col for col in numeri
 if len(features) == 0:
   st.write("Please select at least one feature")
   st.stop()
+
+df = df[features + [target]].dropna()
+
+X = df[features]
+y = df[target]
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+st.subheader("Model Evaludation")
+st.write(f"Mean Squared Error: {mse:.2f}")
+st.write(f"R^2 Score: {r2:.2f}")
+
+
+
+
 
 
 
